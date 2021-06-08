7c9b41ec7bd0722a559b2f54d46db8d1554f5d8f
Nepire <3066574695@qq.com>
2021-06-08 17:09:36 +0800

    新文件：   main 	修改：     main.go

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
+
 package main
 import (
     "encoding/json"
     "net/http"
+    "net/url"
     "fmt"
     "io/ioutil"
     "os"
@@ -17,23 +19,30 @@ type TeaData struct {
     datetime string
 }
 type LAEdata struct {
-    lpm int
-    apm int
+    lpm float32
+    apm float32
     eff float32
 }
 func calc_data(data []TeaData,i int) LAEdata{
     var laedata LAEdata
-    laedata.lpm = 24000*data[i].Pieces/data[i].Time
-    laedata.apm = 60000*data[i].Attack/data[i].Time
-    laedata.eff = float32(laedata.apm)/float32(laedata.lpm)
+    laedata.lpm = 24000*float32(data[i].Pieces)/float32(data[i].Time)
+    laedata.apm = 60000*float32(data[i].Attack)/float32(data[i].Time)
+    laedata.eff = laedata.apm/laedata.lpm
     return laedata
 
 }
 func getjson(name string)  []TeaData{
     var data []TeaData
-    var url string = "http://139.199.75.237:8888/getProfile?id="
-    url += name//os.Args[1]
-    resp, err := http.Get(url)
+    params := url.Values{}
+    Url, err := url.Parse("http://121.4.147.128:8888/getProfile")
+    if err != nil {
+        fmt.Println("some error")
+    }
+    params.Set("id",name)
+    Url.RawQuery = params.Encode()
+    urlPath := Url.String()
+    //fmt.Println(url)
+    resp, err := http.Get(urlPath)
     defer resp.Body.Close()
     body, err := ioutil.ReadAll(resp.Body)
     json.Unmarshal(body, &data)
@@ -45,16 +54,13 @@ func getjson(name string)  []TeaData{
 func main() {
     var data []TeaData
     var laedata LAEdata
-    var now_game int = 0
+    //var now_game int = 0
     var name string = os.Args[1]
     fmt.Printf("LPM\tAPM\tEFF\n")
-    now_game = getjson(name)[0].Idmultiplayergameresult
-    for true {
-        data = getjson(name)
-        if data[0].Idmultiplayergameresult!=now_game{
-            now_game = data[0].Idmultiplayergameresult
-            laedata = calc_data(data,0)
-            fmt.Printf("%d\t%d\t%0.4f\n",laedata.lpm,laedata.apm,laedata.eff)
-        }
+    //now_game = getjson(name)[0].Idmultiplayergameresult
+    data = getjson(name)
+    for i:=0;i<15;i++{
+    laedata = calc_data(data,i)
+    fmt.Printf("%0.3f\t%0.3f\t%0.3f\n",laedata.lpm,laedata.apm,laedata.eff)
     }
 }
